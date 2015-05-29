28a9eea70508ffb0cd712d0f566722c862e7ebe4
Bartek Tomala <bartholomaios@gmail.com>
2015-05-29 22:09:31 +0200

    configurable user agent

--- src/main/scala/btomala/http/mock/helpers/akkahttp/package.scala
+++ src/main/scala/btomala/http/mock/helpers/akkahttp/package.scala
@@ -4,9 +4,10 @@ import akka.http.scaladsl.model.headers.{`User-Agent`, Host}
 
 package object akkahttp {
   /** required headers when you are using akka-http client*/
-  def headers(host: String = "localhost", port: Int = 80) = scala.collection.immutable.Seq(
-    Host(host, port),
-    `User-Agent`("akka-http/2.3.11")
-  )
+  def headers(host: String = "localhost", port: Int = 80, userAgent:String  = "akka-http/2.3.11") =
+    scala.collection.immutable.Seq(
+      Host(host, port),
+      `User-Agent`(userAgent)
+    )
 
 }
--- src/main/scala/btomala/http/mock/helpers/dispatch/package.scala
+++ src/main/scala/btomala/http/mock/helpers/dispatch/package.scala
@@ -5,10 +5,10 @@ import akka.http.scaladsl.model.headers._
 
 package object dispatch {
   /** required headers when you are using dispatch client*/
-  def headers(host: String = "localhost", port: Int = 80) = scala.collection.immutable.Seq(
+  def headers(host: String = "localhost", port: Int = 80, userAgent: String = "Dispatch/0.11.1-SNAPSHOT") = scala.collection.immutable.Seq(
     Host(host, port),
     Connection("keep-alive"),
     Accept(Vector(MediaRanges.`*/*`)),
-    `User-Agent`("Dispatch/0.11.1-SNAPSHOT")
+    `User-Agent`(userAgent)
   )
 }
