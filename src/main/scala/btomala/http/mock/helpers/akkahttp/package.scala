d8ddb0028e9af961c3a064cb15c10141d2769956
Bartek Tomala <bartholomaios@gmail.com>
2015-05-29 08:55:40 +0200

    add few comments

--- src/main/scala/btomala/http/mock/helpers/akkahttp/package.scala
+++ src/main/scala/btomala/http/mock/helpers/akkahttp/package.scala
@@ -3,6 +3,7 @@ package btomala.http.mock.helpers
 import akka.http.scaladsl.model.headers.{`User-Agent`, Host}
 
 package object akkahttp {
+  /** required headers when you are using akka-http client*/
   def headers(host: String = "localhost", port: Int = 80) = scala.collection.immutable.Seq(
     Host(host, port),
     `User-Agent`("akka-http/2.3.11")
--- src/main/scala/btomala/http/mock/helpers/dispatch/package.scala
+++ src/main/scala/btomala/http/mock/helpers/dispatch/package.scala
@@ -4,6 +4,7 @@ import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers._
 
 package object dispatch {
+  /** required headers when you are using dispatch client*/
   def headers(host: String = "localhost", port: Int = 80) = scala.collection.immutable.Seq(
     Host(host, port),
     Connection("keep-alive"),
