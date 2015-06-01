3cb0a0c2a020040cc32741900d7837007bbfd811
Bartek Tomala <bartholomaios@gmail.com>
2015-06-01 21:45:55 +0200

    add little bit documentation

--- src/main/scala/btomala/teapot/response/package.scala
+++ src/main/scala/btomala/teapot/response/package.scala
@@ -2,10 +2,14 @@ package btomala.teapot
 
 import akka.http.scaladsl.model.HttpProtocols.`HTTP/1.0`
 import akka.http.scaladsl.model.{HttpProtocol, HttpResponse}
+import btomala.teapot.headers.default
 import btomala.teapot.status.`I'mATeapot`
 
 package object response {
-  lazy val teapot = HttpResponse(`I'mATeapot`)
-  lazy val oldTeapot = HttpResponse(`I'mATeapot`, protocol = `HTTP/1.0`)
+  /** response for `HTTP/1.1` protocol with stubbed date */
+  lazy val teapot = HttpResponse(`I'mATeapot`, headers = default.response)
+  /** response for `HTTP/1.0` protocol with stubbed date*/
+  lazy val oldTeapot = HttpResponse(`I'mATeapot`, protocol = `HTTP/1.0`, headers = default.response)
+  /** response depend on `HttpProtocol`*/
   def teapotResponse(protocol: HttpProtocol) =  if(protocol == `HTTP/1.0`) oldTeapot else teapot
 }
--- src/main/scala/btomala/teapot/status/package.scala
+++ src/main/scala/btomala/teapot/status/package.scala
@@ -3,5 +3,6 @@ package btomala.teapot
 import akka.http.scaladsl.model.StatusCodes
 
 package object status {
+  /** `HttpStatus` for all misunderstand request to the mock server*/
   val `I'mATeapot` = StatusCodes.custom(418, "I'm a teapot", "The server cannot meet the recorded request.", isSuccess = false, allowsEntity = false)
 }
