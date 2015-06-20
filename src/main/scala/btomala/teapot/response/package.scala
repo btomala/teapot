96c11e99a6cbb5683ff76f5951b40461c13bf670
Bartek Tomala <bartholomaios@gmail.com>
2015-06-20 19:33:18 +0200

    override default value to avoid mistake

--- src/main/scala/btomala/teapot/response/package.scala
+++ src/main/scala/btomala/teapot/response/package.scala
@@ -1,13 +1,13 @@
 package btomala.teapot
 
-import akka.http.scaladsl.model.HttpProtocols.`HTTP/1.0`
+import akka.http.scaladsl.model.HttpProtocols.{`HTTP/1.0`,`HTTP/1.1`}
 import akka.http.scaladsl.model.{HttpProtocol, HttpResponse}
 import btomala.teapot.headers.default
 import btomala.teapot.status.`I'mATeapot`
 
 package object response {
   /** response for `HTTP/1.1` protocol with stubbed date */
-  lazy val teapot = HttpResponse(`I'mATeapot`, headers = default.response)
+  lazy val teapot = HttpResponse(`I'mATeapot`, protocol = `HTTP/1.1`, headers = default.response)
   /** response for `HTTP/1.0` protocol with stubbed date*/
   lazy val oldTeapot = HttpResponse(`I'mATeapot`, protocol = `HTTP/1.0`, headers = default.response)
   /** response depend on `HttpProtocol`*/
